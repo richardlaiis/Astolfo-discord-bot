729990b8eb354ece6d5ef17e8f4d171e0ad8b233
richardlaiis <richardlaiis@proton.me>
2024-07-30 21:38:44 +0800

    slash alt

--- cmds/slash.py
+++ cmds/slash.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from core.classes import cogExtensions
+# from typing import Optional
 # import random
 # import json
 
@@ -16,6 +17,13 @@ class Slash(cogExtensions):
     @app_commands.describe(a = "base", b = "exponent")
     async def pow(self, interaction: discord.Interaction, a: float, b: float) -> None:
         await interaction.response.send_message(f"{a}^{b}={a**b}")
+    
+    # @app_commands.command(name = "say", description = "Say what you want to say.")
+    # @app_commands.describe(user = "name", content = "words")
+    # async def say(self, interaction: discord.Interaction, user: str, content: Optional[str] = None):
+    #     if content == None:
+    #         content = "..."
+    #     await interaction.response.send_message(f"{user} made me say: {content}")
 
 async def setup(bot):
     await bot.add_cog(Slash(bot))
