ea1967296c03f0ffe17c10dead1227db77d5b802
richardlaiis <richardlaiis@proton.me>
2024-08-01 22:46:29 +0800

    add choice-based slash

--- cmds/slash.py
+++ cmds/slash.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+from discord.app_commands import Choice
 from core.classes import cogExtensions
 # from typing import Optional
 # import random
@@ -18,6 +19,29 @@ class Slash(cogExtensions):
     async def pow(self, interaction: discord.Interaction, a: float, b: float) -> None:
         await interaction.response.send_message(f"{a}^{b}={a**b}")
     
+    @app_commands.command(name = 'order', description = "Order something~")
+    @app_commands.describe(meal = "edible thing", size = "how big the things are")
+    @app_commands.choices(
+        meal = [
+            Choice(name = "Hamburger", value = "hamburger"),
+            Choice(name = "Deeznuts", value = "deeznuts"),
+            Choice(name = "Cock", value = "cock"),
+            Choice(name = "Cannabis", value = "cannabis"),
+            Choice(name = "Coke", value = "coke")
+        ],
+        size = [
+            Choice(name = "Big", value = 1),
+            Choice(name = "Medium", value = 2),
+            Choice(name = "Small", value = 3),
+            Choice(name = "Tiny", value = 4)
+        ]
+    )
+    async def order(self, interaction: discord.Interaction, meal: Choice[str], size: Choice[int]) -> None:
+        customer = interaction.user.name
+        meal = meal.value
+        size = size.name
+        await interaction.response.send_message(f"{customer} ordered {size} {meal}")
+
     # @app_commands.command(name = "say", description = "Say what you want to say.")
     # @app_commands.describe(user = "name", content = "words")
     # async def say(self, interaction: discord.Interaction, user: str, content: Optional[str] = None):
